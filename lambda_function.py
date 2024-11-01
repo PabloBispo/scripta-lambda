d47c304e082896cf7c405feea886b66628065a37
Pablo Bispo <pablofernando@live.com>
2024-10-31 23:57:07 -0300

    initial commit

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,4 @@
 def handler(event, context):
     name = event["name"]
 
-    return {"statusCode": 200, "message": f"Hello {name}!"}
+    return {"statusCode": 200, "message": f"Hello {name}! Welcome!"}
