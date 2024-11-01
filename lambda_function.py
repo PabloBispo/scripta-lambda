885e28f5612b3289c43099e4ddc9f755288d9042
Pablo Bispo <pablofernando@live.com>
2024-11-01 00:18:18 -0300

    dummy commit again

--- lambda_function.py
+++ lambda_function.py
@@ -2,6 +2,6 @@ def handler(event, context):
     '''
     handler
     '''
-    name = event["name"]
+    name = event.get("name", "John Doe")
 
     return {"statusCode": 200, "message": f"Hello {name}! Welcome!"}
